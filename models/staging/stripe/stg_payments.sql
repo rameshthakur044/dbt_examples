3b9370f95f2bc7b0f219fd504a880ebea80c785c
trameshsingh044@gmail.com <trameshsingh044@gmail.com>
2023-03-02 05:16:38 +0000

    added test

---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,10 @@
+select 
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+
+    {{ cents_to_dollars('amount', 4) }} amount,
+    created as created_at
+
+from {{ source('stripe', 'payment') }}
